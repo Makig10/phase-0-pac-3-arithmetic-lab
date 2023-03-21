e959e17153990124d2a8aba249e546fee2388df7
Brian <mahuibrian14@gmail.com>
2023-03-21 11:16:47 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,48 @@
+1+80;
+60-40;
+2*3.4;
+5.0/2.5;
+
+function add(a,b) {
+return a+b;
+}
+function subtract(a,b) {
+return a-b;
+}
+function multiply(a,b) {
+return a*b;
+}
+function divide(a,b) {
+return a/b;
+}
+
+let number= 5
+number++;
+number+= 3;
+//console.log(number);
+
+number =10;
+function add5() {
+ return (number += 5);
+}
+//console.log(add5());
+
+function increment(number) {
+ return number +1;
+}
+function decrement(number) {
+ return number -1;
+}
+parseInt("2",10);
+//console.log(parseInt());
+parseInt("nonsense!",10);
+//console.log(parseInt("nonsense!",10));
+parseFloat(2.3555);
+//console.log(parseFloat(2.3555));
+function makeInt(string) {
+   return parseInt(string,10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
+
